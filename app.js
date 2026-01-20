a969c88c9444adf7647129a411c455919fad0ba9
RenoEasy <renoeasycalc@gmail.com>
2026-01-20 09:45:33 +0800

    API 3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // 1. 初始化 Supabase (修正命名衝突)
 // ==========================================
 const SUPABASE_URL = 'https://tjavymruxsqkexczmxvd.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_JV7PTUNvhawsCVANpQ8uNsb_publishable_JV7PTUNvhawsCVANpQ8uNg_zV8qHNAQg_zV8qHNAQ';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqYXZ5bXJ1eHNxa2V4Y3pteHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4MzcxMTksImV4cCI6MjA4MzQxMzExOX0._769_JX4HDWjg01Ch_vhNgVsSSg0vLthHFT6NzB642g';
 
 // ⚠️ 注意：這裡改名叫 supabaseClient，避免跟工具箱名字打架
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
